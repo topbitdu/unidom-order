26a6a2d93bae127992564101980fa2656c138df5
Topbit Du <topbit.du@gmail.com>
2016-12-26 10:48:30 +0800

    1, Improve the Order Adjustment model to configure the Adjustment Factor enum code.

--- app/models/unidom/order/order_adjustment.rb
+++ app/models/unidom/order/order_adjustment.rb
@@ -5,12 +5,15 @@ class Unidom::Order::OrderAdjustment < Unidom::Order::ApplicationRecord
   self.table_name = 'unidom_order_adjustments'
 
   include Unidom::Common::Concerns::ModelExtension
+  include ProgneTapera::EnumCode
 
   validates :amount, presence: true, numericality: { less_than: 1000000000, greater_than: -1000000000 }
 
   belongs_to :adjusted, polymorphic: true
 
   scope :adjusted_is, ->(adjusted) { where adjusted: adjusted }
+
+  code :adjustment_factor, Unidom::Order::AdjustmentFactor
 
   def self.adjust!(adjusted, amount: 0, due_to: 'FRGT', opened_at: Time.now)
     query      = adjusted_is(adjusted).adjustment_factor_coded_as(due_to).valid_at(now: opened_at).alive
