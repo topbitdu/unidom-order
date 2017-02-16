e6aecff2cc13c35acaa7d21674eed6950ee28df3
Topbit Du <topbit.du@gmail.com>
2017-02-16 10:04:05 +0800

    1, Improve the Order Adjustment spec for the validations on the #amount attribute, & the #adjustment_factor_code attribute.

--- lib/rspec/models/unidom/order/order_adjustment_spec.rb
+++ lib/rspec/models/unidom/order/order_adjustment_spec.rb
@@ -18,6 +18,36 @@ describe Unidom::Order::OrderAdjustment, type: :model do
 
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
+    it_behaves_like 'validates', model_attributes, :amount,
+      {                             } => 0,
+      { amount: nil                 } => 2,
+      { amount: ''                  } => 2,
+      { amount: '1'                 } => 0,
+      { amount: 1                   } => 0,
+      { amount: 'A'                 } => 1,
+      { amount: '1.23'              } => 0,
+      { amount: 1.23                } => 0,
+      { amount: '-0.01'             } => 0,
+      { amount: -0.01               } => 0,
+      { amount: '0'                 } => 0,
+      { amount: 0                   } => 0,
+      { amount: '0.01'              } => 0,
+      { amount: 0.01                } => 0,
+      { amount: '999_999_999.99'    } => 0,
+      { amount: 999_999_999.99      } => 0,
+      { amount: '1_000_000_000'     } => 1,
+      { amount: 1_000_000_000       } => 1,
+      { amount: '1_000_000_000.01'  } => 1,
+      { amount: 1_000_000_000.01    } => 1,
+      { amount: '-999_999_999.99'   } => 0,
+      { amount: -999_999_999.99     } => 0,
+      { amount: '-1_000_000_000'    } => 1,
+      { amount: -1_000_000_000      } => 1,
+      { amount: '-1_000_000_000.01' } => 1,
+      { amount: -1_000_000_000.01   } => 1
+
+    it_behaves_like 'ProgneTapera::EnumCode', described_class.new(model_attributes), :adjustment_factor, Unidom::Order::AdjustmentFactor
+
   end
 
 end
