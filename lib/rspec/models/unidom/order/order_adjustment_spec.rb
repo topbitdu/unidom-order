3c3349024560af844829dbc27cf61de73f13ee35
Topbit Du <topbit.du@gmail.com>
2017-03-08 12:20:17 +0800

    1, Improve the Order Adjustment spec for the ``belongs_to :adjusted, polymorphic: true`` macro.

--- lib/rspec/models/unidom/order/order_adjustment_spec.rb
+++ lib/rspec/models/unidom/order/order_adjustment_spec.rb
@@ -48,6 +48,32 @@ describe Unidom::Order::OrderAdjustment, type: :model do
 
     it_behaves_like 'ProgneTapera::EnumCode', described_class.new(model_attributes), :adjustment_factor, Unidom::Order::AdjustmentFactor
 
+    order_attributes = {
+      placer_id:        SecureRandom.uuid,
+      placer_type:      'Unidom::Order::Placer::Mock',
+      taker_id:         SecureRandom.uuid,
+      taker_type:       'Unidom::Order::Taker::Mock',
+      number:           '202001019527',
+      purchase_amount:  10.00,
+      aggregate_amount: 12.00
+    }
+
+    order_item_attributes = {
+      order_id:        SecureRandom.uuid,
+      ordered_id:      SecureRandom.uuid,
+      ordered_type:    'Unidom::Order::Ordered::Mock',
+      placer_id:       SecureRandom.uuid,
+      placer_type:     'Unidom::Order::Placer::Mock',
+      ordinal:         1,
+      unit_price:      10.00,
+      quantity:        12.00,
+      purchase_amount: 120.00,
+      subtotal_amount: 100.00
+    }
+
+    it_behaves_like 'belongs_to', model_attributes, :adjusted, Unidom::Order::Order,     order_attributes
+    it_behaves_like 'belongs_to', model_attributes, :adjusted, Unidom::Order::OrderItem, order_item_attributes
+
   end
 
 end
