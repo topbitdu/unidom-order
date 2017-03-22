951c78135bbc89c798d7c33dea947b549d7abecc
Topbit Du <topbit.du@gmail.com>
2017-03-22 16:39:48 +0800

    1, Improve the Order Adjustment spec for the validations on the #amount attribute.

--- lib/rspec/models/unidom/order/order_adjustment_spec.rb
+++ lib/rspec/models/unidom/order/order_adjustment_spec.rb
@@ -18,33 +18,8 @@ describe Unidom::Order::OrderAdjustment, type: :model do
 
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
-    it_behaves_like 'validates', model_attributes, :amount,
-      {                             } => 0,
-      { amount: nil                 } => 2,
-      { amount: ''                  } => 2,
-      { amount: '1'                 } => 0,
-      { amount: 1                   } => 0,
-      { amount: 'A'                 } => 1,
-      { amount: '1.23'              } => 0,
-      { amount: 1.23                } => 0,
-      { amount: '-0.01'             } => 0,
-      { amount: -0.01               } => 0,
-      { amount: '0'                 } => 0,
-      { amount: 0                   } => 0,
-      { amount: '0.01'              } => 0,
-      { amount: 0.01                } => 0,
-      { amount: '999_999_999.99'    } => 0,
-      { amount: 999_999_999.99      } => 0,
-      { amount: '1_000_000_000'     } => 1,
-      { amount: 1_000_000_000       } => 1,
-      { amount: '1_000_000_000.01'  } => 1,
-      { amount: 1_000_000_000.01    } => 1,
-      { amount: '-999_999_999.99'   } => 0,
-      { amount: -999_999_999.99     } => 0,
-      { amount: '-1_000_000_000'    } => 1,
-      { amount: -1_000_000_000      } => 1,
-      { amount: '-1_000_000_000.01' } => 1,
-      { amount: -1_000_000_000.01   } => 1
+    it_behaves_like 'validates numericality', model_attributes, :amount,
+      range: -1_000_000_000..1_000_000_000, minimum_inclusive: true,  maximum_inclusive: true
 
     it_behaves_like 'ProgneTapera::EnumCode', described_class.new(model_attributes), :adjustment_factor, Unidom::Order::AdjustmentFactor
 
